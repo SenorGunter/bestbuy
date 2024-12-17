d754b5a351cb20b3d0531e70cb34341b5cbc89b1
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2024-12-17 01:36:34 +0100

    create products class

---
+++ products.py
@@ -0,0 +1,44 @@
+
+class Product:
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+
+    def get_quantity(self) -> float:
+        return float(self.quantity)
+
+
+    def set_quantity(self, quantity):
+        if quantity >= 0:
+            self.quantity = quantity
+        else:
+            return "Quantity has to be 0 or higher"
+
+
+    def is_active(self) -> bool:
+        if self.active is True:
+            return True
+        else:
+            return False
+
+
+    def activate(self):
+        self.active = True
+
+
+    def deactivate(self):
+        self.active = False
+
+
+    def show(self) -> str:
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+
+    def buy(self, quantity) -> float:
+        if quantity <= self.quantity:
+            self.set_quantity(quantity)
+            return float(quantity * self.price)
